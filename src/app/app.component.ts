0217785faaf5559d0a3513304f47505cd6927871
Jianmin Wei <jianminwei1@gmail.com>
2023-01-02 14:06:25 -0500

    Added localStorage to the app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { localStorageToken } from './localstorage.token';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit{
   title = 'hotelinventoryapp';
+
+  constructor(@Inject(localStorageToken) private localStorage: Storage) {
+
+  }
+
+  ngOnInit(): void {
+
+    //add an item to the localStorate.
+    this.localStorage.setItem('name', 'Hilton Hotel');
+  }
 }
