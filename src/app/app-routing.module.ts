678482a9d87a177bc4269621b6c1a56945e070f2
Jianmin Wei <jianminwei1@gmail.com>
2023-01-05 06:37:45 -0500

    Added first Routing service in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoomsComponent } from './rooms/rooms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'rooms', component: RoomsComponent},
+  {path: '', redirectTo: 'rooms', pathMatch: 'full'},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
